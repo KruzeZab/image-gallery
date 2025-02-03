9e4fe4c0469e10ffab09dc1aa519f82e4f6e2cf4
Kruze Zab <kruzezab4@gmail.com>
2025-02-03 10:28:54 +0545

    increase skeleton count

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -2,6 +2,6 @@ export const DEFAULT_PAGE_START = 1;
 
 export const DEFAULT_PAGE_LIMIT = 20;
 
-export const DEFAULT_IMAGE_SKELETON_COUNT = 8;
+export const DEFAULT_IMAGE_SKELETON_COUNT = 12;
 
 export const SEARCH_DEBOUNCE_TIMEOUT = 300;
